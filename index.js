d3f1dcc95bb7cb08b3b843a1cc6666f57d98948c
masumbillahwebdeveloper <masumbillah.webdeveloper@gmail.com>
2023-06-10 21:25:47 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req,res,next)=>{
+    const authorization = req.headers.authorization;
+    if(!authorization){
+      return res.status(401).send({error:true,message:'Un authorized access'});
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+      if(err){
+        return res.status(401).send({error:true,message:'Un authorized access'})
+      }
+      req.decoded = decoded;
+      next();
+    })
+  }
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -28,12 +44,19 @@ async function run() {
         await client.connect();
         const userCollection = client.db("martialDb").collection("user");
         const classCollection = client.db("martialDb").collection("class");
+
+
+        app.post('/jwt', (req,res)=>{
+            const user = req.body;
+            const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+            res.send({token})
+          })
         //users related api
-        app.get('/users', async (req, res) => {
+        app.get('/users',verifyJWT, async (req, res) => {
             const result = await userCollection.find().toArray();
             res.send(result);
         })
-        app.post('/users', async (req, res) => {
+        app.post('/users',  async (req, res) => {
             const user = req.body;
             console.log(user);
             const query = { email: user.email }
